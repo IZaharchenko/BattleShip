0b25a0ec49f859c6ffb030fb70a7287cb4ac1125
Zaharchenko <ununnillium@rambler.ru>
2017-06-29 00:51:37 +0300

    replace "Coordinates" to int, int

--- src/BattleShipGame/BattleShipGame/ConsoleView.cpp
+++ src/BattleShipGame/BattleShipGame/ConsoleView.cpp
@@ -3,14 +3,14 @@
 
 using std::cout;
 
-void ConsoleView::setCoordinates(const Coordinates& c)
+void ConsoleView::setCoordinates(int x, int y)
 {
-	COORD coord{ static_cast<short>(c.getX()), static_cast<short>(c.getY()) };
+	COORD coord{ static_cast<short>(x), static_cast<short>(y) };
 	SetConsoleCursorPosition(getHandleOut(), coord);
 }
 void ConsoleView::print(const Coordinates& coordinates, const string state)
 {
-	setCoordinates(coordinates);
+	setCoordinates(coordinates.getX(), coordinates.getY());
 	cout << state;
 }
 const HANDLE& ConsoleView::getHandleOut()
