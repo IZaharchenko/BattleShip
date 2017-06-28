1c241cdd27e232435b32e19d74ceb9152e8a0ae0
Zaharchenko <ununnillium@rambler.ru>
2017-06-29 00:49:53 +0300

    delete magic number

---
+++ src/BattleShipGame/BattleShipGame/KeyCode.h
@@ -0,0 +1,11 @@
+#ifndef _KEY_H_
+#define _KEY_H_
+
+const unsigned int enter = 13;
+const unsigned int esc = 27;
+const unsigned int up = 72;
+const unsigned int left = 75;
+const unsigned int right = 77;
+const unsigned int down = 80;
+
+#endif //_KEY_H_
